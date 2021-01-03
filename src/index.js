871512e4a5aa6b4f1aa4399c8ec544f6c4d34b49
Jino Chun <jukoku@gmail.com>
2021-01-03 16:57:51 +0900

    Vanilla Redux addEvent

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
